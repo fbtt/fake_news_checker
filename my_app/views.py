ab1e59c73259924458ebaecc7759ee87126c835f
fbtt <ferbattisti94@gmail.com>
2020-08-01 19:08:38 -0300

    only to tribo_tech review

--- my_app/views.py
+++ my_app/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, template_name='base.html')
+
+
+def new_check(request):
+
+    news_content = request.POST.get('news_content')
+
+    # Consultar modelo e retornar veracidade
+    veracidade = 'FALSA'
+
+    stuff_for_frontend = {
+        'veracidade': veracidade,
+    }
+
+    return render(request, 'my_app/new_check.html', stuff_for_frontend)
